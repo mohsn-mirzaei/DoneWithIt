83ebf148b7470ef2a68d0f254653777e6ec4e77a
Mohsen Mirzaei <mohsn.mirzaei@gmail.com>
2023-10-30 14:49:03 +0330

    Building MessagesScreen & use FlatList

--- App.js
+++ App.js
@@ -5,7 +5,8 @@ import WelcomeScreen from "./app/screens/WelcomeScreen";
 import AppButton from "./app/components/AppButton";
 import Card from "./app/components/Card";
 import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
+import MessagesScreen from "./app/screens/MessagesScreen";
 
 export default App = () => {
-  return <ViewImageScreen />;
+  return <MessagesScreen />;
 };
---
+++ app/screens/MessagesScreen.js
@@ -0,0 +1,34 @@
+import { FlatList } from "react-native";
+import ListItem from "../components/ListItem";
+
+const messages = [
+  {
+    id: 1,
+    fullname: "Mohsen Mirzaei",
+    description: "My name is mohsen, I'm Frontend developer",
+    image: require("../assets/mosh.jpg"),
+  },
+  {
+    id: 2,
+    fullname: "Saeid Tavazani",
+    description: "My name is saeid, I'm Backend developer",
+    image: require("../assets/mosh.jpg"),
+  },
+];
+const MessagesScreen = () => {
+  return (
+    <FlatList
+      data={messages}
+      keyExtractor={(message) => message.id.toString()}
+      renderItem={({ item }) => (
+        <ListItem
+          fullname={item.fullname}
+          description={item.description}
+          image={item.image}
+        />
+      )}
+    />
+  );
+};
+
+export default MessagesScreen;
