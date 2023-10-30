bd95b460f5b699e95a7da83a117c69c370f074c2
Mohsen Mirzaei <mohsn.mirzaei@gmail.com>
2023-10-30 12:13:53 +0330

    Building Button component

--- App.js
+++ App.js
@@ -1,7 +1,19 @@
+import { View } from "react-native";
 import AppText from "./app/components/AppText";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
+import AppButton from "./app/components/AppButton";
 
 export default function App() {
-  return <AppText>Hello React Native</AppText>;
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <AppButton title="Login" onPress={() => console.log("touched")} />
+    </View>
+  );
 }
---
+++ app/components/AppButton/AppButton.js
@@ -0,0 +1,13 @@
+import { Text, TouchableOpacity } from "react-native";
+
+import styles from "./styles";
+
+const AppButton = ({ title, onPress }) => {
+  return (
+    <TouchableOpacity style={styles.button} onPress={onPress}>
+      <Text style={styles.text}>{title}</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default AppButton;
