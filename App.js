b1cfa04694f4ceb8760c73e6c6cd87acbb95ef81
Mohsen Mirzaei <mohsn.mirzaei@gmail.com>
2023-11-01 11:41:53 +0330

    Building FormImagePicker

--- App.js
+++ App.js
@@ -1,25 +1,10 @@
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import { useState } from "react";
-import ImageInputList from "./app/components/ImageInputList";
+import FormImagePicker from "./app/components/forms/FormImagePicker";
 
 export default App = () => {
-  const [imageUris, setImageUris] = useState([]);
-
-  const handleRemove = (uri) => {
-    setImageUris(imageUris.filter((imageUri) => imageUri !== uri));
-  };
-
-  const handleAdd = (uri) => {
-    setImageUris([...imageUris, uri]);
-  };
-
   return (
     <GestureHandlerRootView>
-      <ImageInputList
-        imageUris={imageUris}
-        onRemoveImage={handleRemove}
-        onAddImage={handleAdd}
-      />
+      <FormImagePicker />
     </GestureHandlerRootView>
   );
 };
---
+++ app/components/forms/FormImagePicker.js
@@ -0,0 +1,33 @@
+import { useFormikContext } from "formik";
+
+import ImageInputList from "../ImageInputList";
+import ErrorMessage from "./ErrorMessage";
+
+const FormImagePicker = ({ name }) => {
+  const { errors, setFieldValue, touched, values } = useFormikContext();
+  const imageUris = values[name];
+
+  const handleRemove = (uri) => {
+    setFieldValue(
+      name,
+      imageUris.filter((imageUri) => imageUri !== uri)
+    );
+  };
+
+  const handleAdd = (uri) => {
+    setFieldValue(name, [...imageUris, uri]);
+  };
+
+  return (
+    <View>
+      <ImageInputList
+        imageUris={imageUris}
+        onRemoveImage={handleRemove}
+        onAddImage={handleAdd}
+      />
+      <ErrorMessage error={errors[name]} visible={touched[name]} />
+    </View>
+  );
+};
+
+export default FormImagePicker;
