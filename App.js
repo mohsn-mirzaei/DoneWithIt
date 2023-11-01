fff7de822aa12458fe31c05cf83ee73fd57c7be1
Mohsen Mirzaei <mohsn.mirzaei@gmail.com>
2023-11-01 10:53:34 +0330

    Building ImageInput - touched

--- App.js
+++ App.js
@@ -1,30 +1,16 @@
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import ImageInput from "./app/components/ImageInput";
-import { Button } from "react-native";
-import * as ImagePicker from "expo-image-picker";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export default App = () => {
   const [imageUri, setImageUri] = useState();
-  const requestPermision = async () => {
-    const result = await ImagePicker.requestCameraPermissionsAsync();
-    if (!result.granted)
-      alert("You need to enable permision to access the library");
-  };
-
-  useEffect(() => {
-    requestPermision();
-  }, []);
-
-  const selectImage = async () => {
-    const result = await ImagePicker.launchImageLibraryAsync();
-    if (!result.canceled) setImageUri(result.uri);
-  };
 
   return (
     <GestureHandlerRootView>
-      <Button title="select image" onPress={selectImage} />
-      <ImageInput imageUri={imageUri} />
+      <ImageInput
+        imageUri={imageUri}
+        onChangeImage={(uri) => setImageUri(uri)}
+      />
     </GestureHandlerRootView>
   );
 };
--- app/components/ImageInput.js
+++ app/components/ImageInput.js
@@ -1,16 +1,63 @@
-import { Image, StyleSheet, View } from "react-native";
+import { useEffect } from "react";
+import {
+  Alert,
+  Image,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
+import * as ImagePicker from "expo-image-picker";
+
 import colors from "../config/colors";
 
-const ImageInput = ({ imageUri }) => {
+const ImageInput = ({ imageUri, onChangeImage }) => {
+  useEffect(() => {
+    requestPermision();
+  }, []);
+
+  const requestPermision = async () => {
+    const result = await ImagePicker.requestCameraPermissionsAsync();
+    if (!result.granted)
+      alert("You need to enable permision to access the library");
+  };
+
+  const handlePress = () => {
+    if (!imageUri) selectImage();
+    else {
+      Alert.alert("Delete", "Are you sure you want delete this image?", [
+        { text: "Yes", onPress: () => onChangeImage(null) },
+        { text: "No" },
+      ]);
+    }
+  };
+
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 0.5,
+      });
+      if (!result.canceled) onChangeImage(result.uri);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      {imageUri ? (
-        <Image style={styles.image} source={{ uri: imageUri }} />
-      ) : (
-        <MaterialCommunityIcons name="camera" size={40} color={colors.medium} />
-      )}
-    </View>
+    <TouchableWithoutFeedback onPress={handlePress}>
+      <View style={styles.container}>
+        {imageUri ? (
+          <Image style={styles.image} source={{ uri: imageUri }} />
+        ) : (
+          <MaterialCommunityIcons
+            name="camera"
+            size={40}
+            color={colors.medium}
+          />
+        )}
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
 
