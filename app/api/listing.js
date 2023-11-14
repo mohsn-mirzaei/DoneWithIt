53ae702059b83f118bf0c0752a2f6dee4f2eeafb
Mohsen Mirzaei <mohsn.mirzaei@gmail.com>
2023-11-14 17:57:47 +0330

    Extending the API layer

--- app/api/listing.js
+++ app/api/listing.js
@@ -4,6 +4,29 @@ const endpoint = "/listings";
 
 const getListing = () => apiClient.get(endpoint);
 
+const addListig = (listing) => {
+  const data = new FormData();
+  data.append("title", listing.title);
+  data.append("price", listing.price);
+  data.append("categoryId", listing.category);
+  data.append("categoryId", listing.category.value);
+  data.append("description", listing.description);
+
+  listing.images.forEach((index, image) =>
+    data.append("images", {
+      name: "image" + index,
+      type: "image/jpeg",
+      uri: image,
+    })
+  );
+
+  if (listing.location)
+    return data.append("location", JSON.stringify(listing.location));
+
+  return apiClient.post(endpoint, data);
+};
+
 export default {
+  addListig,
   getListing,
 };
