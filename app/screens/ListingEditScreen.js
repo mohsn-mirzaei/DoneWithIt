3f352ad7b0d2e1f2604fb1a2e82a0655a14c79e7
Mohsen Mirzaei <mohsn.mirzaei@gmail.com>
2023-11-27 12:29:58 +0330

    Adding a Progress Bar

--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -86,6 +86,7 @@ const ListingEditScreen = () => {
   const [progress, setProgress] = useState(0);
 
   const handleSubmit = async (listing) => {
+    setProgress(0);
     setUploadVisible(true);
     const result = await listingApi.addListing(
       { ...listing, location },
--- app/screens/UploadScreen.js
+++ app/screens/UploadScreen.js
@@ -1,11 +1,12 @@
 import { Modal, StyleSheet, View } from "react-native";
-import Text from "../components/Text";
+import * as Progress from "react-native-progress";
+import colors from "../config/colors";
 
 const UploadScreen = ({ progress = 0, visible = false }) => {
   return (
     <Modal visible={visible}>
       <View style={styles.container}>
-        <Text>{progress * 100}%</Text>
+        <Progress.Bar color={colors.primary} progress={progress} width={200} />
       </View>
     </Modal>
   );
