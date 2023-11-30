13cf71b3ad1d38320eae8e13aeecf3ea0cef0dbe
Mohsen Mirzaei <mohsn.mirzaei@gmail.com>
2023-11-30 19:20:04 +0330

    Controlling the Splash Screen

--- App.js
+++ App.js
@@ -1,32 +1,46 @@
-import { useEffect } from "react";
+import { useEffect, useCallback, useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import jwtDecode from "jwt-decode";
+import * as SplashScreen from "expo-splash-screen";
 
 import AuthContext from "./app/auth/context";
 import AuthNavigator from "./app/components/navigation/AuthNavigator";
 import AppNavigator from "./app/components/navigation/AppNavigator";
 import navigationTheme from "./app/components/navigation/navigationTheme";
 import OfflineNotice from "./app/components/OfflineNotice";
-import { useState } from "react";
 import authStorage from "./app/auth/storage";
+
+SplashScreen.preventAutoHideAsync();
 
 export default App = () => {
   const [user, setUser] = useState();
+  const [appIsReady, setAppIsReady] = useState(false);
 
   const restoreToken = async () => {
     const token = await authStorage.getToken();
-    if (!token) return;
+    if (!token) return setAppIsReady(true);
     setUser(jwtDecode(token));
+    setAppIsReady(true);
   };
 
   useEffect(() => {
     restoreToken();
   }, []);
 
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
   return (
     <AuthContext.Provider value={{ user, setUser }}>
-      <GestureHandlerRootView style={{ flex: 1 }}>
+      <GestureHandlerRootView style={{ flex: 1 }} onLayout={onLayoutRootView}>
         <OfflineNotice />
         <NavigationContainer theme={navigationTheme}>
           {user ? <AppNavigator /> : <AuthNavigator />}
